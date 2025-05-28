c625a708a6bd1d402b9d2d6d6c190fa86d86bd53
Harold Caminero <jayrold@gmail.com>
2025-05-28 20:39:34 +0800

    feat: Setup tailwind and basic page

---
+++ src/app/_ui/header.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function Header(): ReactElement {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+}
---
+++ src/app/_ui/hero.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function Hero(): ReactElement {
+    return (
+        <section>
+            <h1>Hero</h1>
+        </section>
+    )
+}
